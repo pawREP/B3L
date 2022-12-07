79b264e19675bbaec7938bffb13a7577c5030bac
pawREP <pawREP@users.noreply.github.com>
2022-12-08 00:54:37 +0100

    Add B3L_NEVERINLINE define

--- include/B3L/Define.h
+++ include/B3L/Define.h
@@ -14,6 +14,7 @@ private:                            \
 
 #if _MSC_VER
     #define B3L_FORCEINLINE __forceinline
+    #define B3L_NEVERINLINE __declspec(noinline)
     #if _MSC_VER >= 1929 
         #define B3L_NO_UNIQUE_ADDRESS [[msvc::no_unique_address]]
     #else
@@ -21,5 +22,6 @@ private:                            \
     #endif
 #else
     #define B3L_FORCEINLINE __attribute__((always_inline))
+    #define B3L_NEVERINLINE __attribute__((noinline))
     #define B3L_NO_UNIQUE_ADDRESS [[no_unique_address]]
 #endif
