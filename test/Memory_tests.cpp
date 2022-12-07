cafe78663e8a155a9c0629b77461a1693455a9c0
pawREP <pawREP@users.noreply.github.com>
2022-12-08 00:32:05 +0100

    Fix page protection bug caused by bad assert usage
    Removes conditional assert around VirtualProtect call. The expression in an assert doesn't get executed in release build which causes a page protection bug

--- test/Memory_tests.cpp
+++ test/Memory_tests.cpp
@@ -10,13 +10,13 @@ TEST(MemoryTests, WriteProtectedMemory) {
     *reinterpret_cast<intptr_t*>(mem) = 0;
 
     DWORD old;
-    assert(VirtualProtect(mem, sizeof(intptr_t), PAGE_READONLY, &old));
+    VirtualProtect(mem, sizeof(intptr_t), PAGE_READONLY, &old);
 
     intptr_t deepPtrSollValue = 123;
     Memory::writeProtectedMemory(mem, deepPtrSollValue);
 
     // Check if protection is unchanged
-    assert(VirtualProtect(mem, sizeof(intptr_t), PAGE_READONLY, &old));
+    VirtualProtect(mem, sizeof(intptr_t), PAGE_READONLY, &old);
     EXPECT_EQ(old, PAGE_READONLY);
 
     EXPECT_EQ(*reinterpret_cast<intptr_t*>(mem), deepPtrSollValue);
